9f456ba2a5a324826f8a1572683f495161fd5479
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-08 14:16:13 +0530

    more "for"

--- 02-programming-constructs/03-for.go
+++ 02-programming-constructs/03-for.go
@@ -3,15 +3,62 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("for[v1]")
 	for i := 0; i < 10; i++ {
 		fmt.Println("i =", i)
 	}
 
 	fmt.Println("for[v2] (while)")
-	no := 1
-	for no < 100 {
+	/*
+		no := 1
+		for no < 100 {
+			no += no
+		}
+		fmt.Println("no :", no)
+	*/
+
+	for no := 1; no < 100; {
 		no += no
+		fmt.Println("no :", no)
 	}
-	fmt.Println("no :", no)
+
+	fmt.Println("for[v3] (infinite)")
+	/*
+		no := 1
+		for {
+			no += no
+			fmt.Println("no :", no)
+			if no >= 128 {
+				break
+			}
+		}
+	*/
+
+	for no := 1; ; {
+		no += no
+		fmt.Println("no :", no)
+		/*
+			if no >= 128 {
+				break
+			}
+		*/
+		if no < 128 {
+			continue
+		}
+		break
+	}
+
+	// Labels: To control the outer loop from an inner loop
+
+I_LOOP:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			fmt.Printf("i = %d, j = %d\n", i, j)
+			if i == j {
+				fmt.Println("================")
+				continue I_LOOP
+			}
+		}
+	}
 }
