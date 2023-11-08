a2563c4d595ad0e0e4689ad60fdb5599dc4c27b0
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-08 14:58:31 +0530

    assignment 01 & 02 solutions

--- 02-programming-constructs/assignment-01.go
+++ 02-programming-constructs/assignment-01.go
@@ -1,1 +1,17 @@
 /* Write a program that prints the prime numbers between 2 to 100 */
+
+package main
+
+import "fmt"
+
+func main() {
+LOOP:
+	for no := 2; no <= 100; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Printf("Prime : %d\n", no)
+	}
+}
--- 02-programming-constructs/assignment-02.go
+++ 02-programming-constructs/assignment-02.go
@@ -23,3 +23,40 @@ if the user input is not (1-5)
 	print "Invalid choice"
 	IMPORTANT : display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, x, y, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice :")
+		fmt.Scanln(&userChoice)
+		if userChoice == 5 {
+			break
+		}
+		if userChoice < 1 || userChoice > 4 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		fmt.Println("Enter the operands:")
+		fmt.Scanln(&x, &y)
+		switch userChoice {
+		case 1:
+			result = x + y
+		case 2:
+			result = x - y
+		case 3:
+			result = x * y
+		case 4:
+			result = x / y
+		}
+		fmt.Println("Result :", result)
+	}
+}
