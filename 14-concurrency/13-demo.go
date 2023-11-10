fc16e14ff03bb674115d4268c5960216c60bd98c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-10 16:04:51 +0530

    send only & receive only channels

--- 14-concurrency/13-demo.go
+++ 14-concurrency/13-demo.go
@@ -12,21 +12,21 @@ import (
 
 // consumer
 func main() {
-	/*
-		var ch chan int
-		ch = make(chan int)
-	*/
-	ch := make(chan int)
+
 	fmt.Println("main started")
-	go add(100, 200, ch)
+	ch := add(100, 200)
 	result := <-ch
 	fmt.Println("result :", result)
 	fmt.Println("main completed")
 }
 
 // producer
-func add(x, y int, ch chan<- int) {
-	time.Sleep(5 * time.Second)
-	result := x + y
-	ch <- result // send the result through the channel
+func add(x, y int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		time.Sleep(5 * time.Second)
+		result := x + y
+		ch <- result
+	}()
+	return ch
 }
