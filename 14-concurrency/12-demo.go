cd415058917fdacc0b5e7351f4d552579dbf0768
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-10 15:57:29 +0530

    updates

--- 14-concurrency/12-demo.go
+++ 14-concurrency/12-demo.go
@@ -1,10 +1,12 @@
-/* communication between goroutines (using share memory by communicating [channels]) */
+/*
+communication between goroutines (using share memory by communicating [channels])
+channels can also be used for synchronizing goroutines
+*/
 
 package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -14,19 +16,15 @@ func main() {
 		ch = make(chan int)
 	*/
 	ch := make(chan int)
-	wg := &sync.WaitGroup{}
 	fmt.Println("main started")
-	wg.Add(1)
-	go add(100, 200, wg, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println("result :", result)
 	fmt.Println("main completed")
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	time.Sleep(5 * time.Second)
 	result := x + y
 	ch <- result // send the result through the channel
-	wg.Done()
 }
