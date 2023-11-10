9529fb3dd4977eb098a143261915803da7212cad
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-10 15:53:35 +0530

    channel behaviors - updated

--- 14-concurrency/11-demo.go
+++ 14-concurrency/11-demo.go
@@ -2,15 +2,31 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
 	ch := make(chan int)
 	go func() {
 		ch <- 100
 	}()
 	data := <-ch
 	fmt.Println(data)
+
+	/*
+		ch := make(chan int)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			data := <-ch
+			fmt.Println(data)
+			wg.Done()
+		}()
+		ch <- 100
+		wg.Wait()
+	*/
 }
 
 // modify the above to perform the "receive" operation in a goroutine
